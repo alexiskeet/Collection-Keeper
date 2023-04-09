7a711cdb1abc680782771fbd7618dc7004724fb2
Alexis <alexis_keeter@yahoo.com>
2023-04-08 22:57:15 -0700

    added collections file

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Product = require('../../models/Product');
+const { Product } = require('../../models/Product');
 
 //route to create/add a product
 router.post('/', async (req,res) => {
@@ -16,5 +16,41 @@ router.post('/', async (req,res) => {
     }
 });
 
-//File is not finished yet
-//add put connection for updating cards
+// File is not finished yet
+// add put connection for updating cards
+
+router.put('/:id', async (req,res) => {
+  try {
+      const product = await Product.update(
+          {
+          product_name: req.body.product_name,
+          description: req.bosy.description,
+          product_date: req.body.product_date,
+          product_location: req.body.product_location,
+          },
+          {
+              where: {
+                  id: req.params.id,
+              },
+          }
+      );
+      res.status(200).json(product);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
+
+// delete a card
+router.delete('/:id', async (req,res) => {
+    try{
+        const deletedProducts = await Product.destroy({
+            where: {
+                id: req.params.id,
+            },
+        })
+    res.status(200).json({message: "Successfully deleted collection item"})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+module.exports = router;
---
+++ models/Collection.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Product extends Model {}
+
+Product.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'collection',
+    }
+);
+
+module.exports = Product;
