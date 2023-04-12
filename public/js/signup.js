7c8cb1d096da974f8dd093cf658ab4c4b3a8ac66
jeannav <120337476+jeannav@users.noreply.github.com>
2023-04-11 20:13:16 -0700

    fixed code for login and register

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,8 +4,9 @@ async function signupFormHandler(event) {
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-
+    console.log(username)
     if (username && email && password) {
+      
         const response = await fetch('/api/users/signup', {
           method: 'post',
           body: JSON.stringify({
@@ -15,7 +16,7 @@ async function signupFormHandler(event) {
           }),
           headers: { 'Content-Type': 'application/json' }
         });
-    
+        console.log(response);
         if (response.ok) {
           console.log('success');
           document.location.replace('/');
--- utils/helpers.js
+++ utils/helpers.js
@@ -9,14 +9,13 @@ module.exports = {
   },
   get_emoji: () => {
     const randomNum = Math.random();
-
     // Return a random emoji
     if (randomNum > 0.7) {
-      return `<span for="img" aria-label="lightbulb">💡</span>`;
+      return `<span for="img" aria-label="lightbulb">:bulb:</span>`;
     } else if (randomNum > 0.4) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
+      return `<span for="img" aria-label="laptop">:computer:</span>`;
     } else {
-      return `<span for="img" aria-label="gear">⚙️</span>`;
+      return `<span for="img" aria-label="gear">:gear:</span>`;
     }
   },
 };
